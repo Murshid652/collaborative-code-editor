7b549b6088cacaddc549a6b6989ad847fef9ba9c
Murshid Raja <murshidraja652@gmil.com@gmail.com>
2022-12-05 01:26:39 +0530

    finally completed :)

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/ACTIONS');
+const cors = require("cors");
+const Axios = require("axios");
+
+
+app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 
@@ -81,6 +87,76 @@ io.on('connection', (socket) => {
 });
 
 
+app.post("/compile", (req, res) => {
+    //getting the required data from the request
+    let code = req.body.code;
+    let language = req.body.language;
+    let input = req.body.input;
+
+    // if (language === "python") {
+    //     language="py"
+    // }
+
+    let data = ({
+        "script": code,
+        "language": language,
+        "versionIndex": "1",
+        "clientId": "dd9e65f02a57d858f1bac2c2ddf85433",
+        "clientSecret": "20e51818f51c5bf3d0df6e1e29d2d493e2e1d2457defec6b9fd12df208a52316",
+        "stdin": input
+    });
+
+
+    //     "code": code,
+    //     "language": language,
+    //     "input": input
+    // "script": code,
+    // "language": language,
+    // "versionIndex": "1",
+    // "clientId": 'dd9e65f02a57d858f1bac2c2ddf85433',
+    // "clientSecret": '20e51818f51c5bf3d0df6e1e29d2d493e2e1d2457defec6b9fd12df208a52316'
+
+    // });
+
+    let config = {
+        method: 'post',
+        url: 'https://api.jdoodle.com/v1/execute',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+
+    // let program = {
+    //     script: code,
+    //     language: language,
+    //     versionIndex: "1",
+    //     clientId: "dd9e65f02a57d858f1bac2c2ddf85433",
+    //     clientSecret: "20e51818f51c5bf3d0df6e1e29d2d493e2e1d2457defec6b9fd12df208a52316"
+    // };
+    // request({
+    //         url: 'https://api.jdoodle.com/v1/execute',
+    //         method: "POST",
+    //         json: program
+    //     },
+    //     function(error, response, body) {
+    //         console.log('error:', error);
+    //         console.log('statusCode:', response && response.statusCode);
+    //         console.log('body:', body);
+    //     })
+
+
+    //calling the code compilation API
+    Axios(config)
+        .then((response) => {
+            res.send(response.data)
+            console.log("data", response.data)
+        }).catch((error) => {
+            res.send("network error\n");
+            console.log("error", error);
+        });
+})
 
 
 const PORT = process.env.PORT || 5000;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -9,114 +9,126 @@ import { Navigate, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
+import Axios from "axios";
 
 const EditorPage = () => {
-
-
-
   const socketRef = useRef(null);
   const codeRef = useRef(null);
   const location = useLocation();
   const reactNavigator = useNavigate();
   const [Clients, setClients] = useState([]);
-  const {RoomId} = useParams();
+  const { RoomId } = useParams();
   console.log(RoomId);
 
-
   useEffect(() => {
-    const init = async ()=> {
+    const init = async () => {
       socketRef.current = await initSocket();
 
-      socketRef.current.on('connect_error' ,(err) => handleErrors(err));
-      socketRef.current.on('connect_failed' ,(err) => handleErrors(err));
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
 
       function handleErrors(e) {
-        console.log('Socket Error' , e);
-        toast.error('Socket connection failed, try again later.');
-        reactNavigator('/');
+        console.log("Socket Error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
       }
 
-      socketRef.current.emit(ACTIONS.JOIN , {
+      socketRef.current.emit(ACTIONS.JOIN, {
         RoomId,
         UserName: location.state?.UserName,
       });
 
-
       //listening for join event
 
-      socketRef.current.on(ACTIONS.JOINED, ({clients , UserName , socketID }) =>{
-        if(UserName !== location.state?.UserName){
-          toast.success(UserName + ' joined the Room');
-          console.log(UserName, " joined");
-        }
-        setClients(clients);
-        // console.log(clients);
-        socketRef.current.emit(ACTIONS.SYNC_CODE, {
-          code : codeRef.current,
-          socketID,
-         });
-      });
-
-      //listening for disconnect event
-      // socketRef.current.on(ACTIONS.DISCONNECTED, ({socketID ,UserName}) => {
-      //   toast.success(UserName + "left the room");
-      //   setClients((prev) => {
-      //     return  prev.filter(
-      //       (client) => client.socketID !== socketID
-      //   )
-      //   })
-      // });
-
       socketRef.current.on(
-        ACTIONS.DISCONNECTED,
-        ({ socketId, UserName }) => {
-            toast.success(UserName + " left the room.")
-            setClients((prev) => {
-                return  prev.filter(
-                    (client) => client.socketID !== socketId
-                  )   
-            });
+        ACTIONS.JOINED,
+        ({ clients, UserName, socketID }) => {
+          if (UserName !== location.state?.UserName) {
+            toast.success(UserName + " joined the Room");
+            console.log(UserName, " joined");
+          }
+          setClients(clients);
+          // console.log(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketID,
+          });
         }
-    );
+      );
 
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, UserName }) => {
+        toast.success(UserName + " left the room.");
+        setClients((prev) => {
+          return prev.filter((client) => client.socketID !== socketId);
+        });
+      });
     };
     init();
 
-    return() => {
+    return () => {
       socketRef.current.disconnect();
       socketRef.current.off(ACTIONS.JOINED);
       socketRef.current.off(ACTIONS.DISCONNECTED);
-    }
-  },[]);
-
+    };
+  }, []);
 
   async function copyRoomId() {
-    try{
+    try {
       await navigator.clipboard.writeText(RoomId);
-      toast.success('Room Id copied');
-    }catch(err)
-    {
-      toast.error('Could not copy Room Id');
+      toast.success("Room Id copied");
+    } catch (err) {
+      toast.error("Could not copy Room Id");
     }
   }
 
-
   async function leaveRoom() {
-    reactNavigator('/')
-  } 
-
+    reactNavigator("/");
+  }
 
   const [userInput, setUserInput] = useState("");
   const [userOutput, setUserOutput] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function compile() {
+    setLoading(true);
+
+    var Code=codeRef.current;
+
+    if (Code === ``) {
+      return;
+    }
+
+    Axios.post(`http://localhost:5000/compile`, {
+      code: Code,
+      language: "cpp",
+      input: userInput,
+    })
+      .then(
+        (res) => {
+          if (res.data.output) {
+            setUserOutput(res.data.output);
+          } else {
+            toast.error("Network error in compilation");
+            setUserOutput(res.data);
+          }
+        }
+        // console.log(codeRef.current)
+      )
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .then(() => {
+        setLoading(false);
+      });
+  }
+
   function clearOutput() {
     setUserOutput("");
   }
 
-
-  if(!location.state){
-    return <Navigate to="/" />
+  if (!location.state) {
+    return <Navigate to="/" />;
   }
-  
 
   return (
     <div className="EditorPage">
@@ -125,46 +137,59 @@ const EditorPage = () => {
           <h2 className="connClients">CONNECTED</h2>
 
           <div className="ClientList">
-            {
-            Clients.map((client) => (
+            {Clients.map((client) => (
               <Client key={client.socketID} username={client.UserName} />
             ))}
-
           </div>
         </div>
 
-          <button className="btn copybtn" onClick={copyRoomId}> Copy Room ID</button>
-          <button className="btn leavebtn" onClick={leaveRoom}> Leave</button>
+        <button className="btn copybtn" onClick={copyRoomId}>
+          {" "}
+          Copy Room ID
+        </button>
+        <button className="btn leavebtn" onClick={leaveRoom}>
+          {" "}
+          Leave
+        </button>
       </div>
 
       <div className="Editor">
-        <Editor socketRef={socketRef} RoomId={RoomId} onCodeChange = {(code) => {codeRef.current = code}} />
+        <Editor
+          socketRef={socketRef}
+          RoomId={RoomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        />
 
-
-        
         <div className="right-container">
           <h4>Input:</h4>
           <div className="input-box">
-            <textarea id="code-inp" onChange=
-              {(e) => setUserInput(e.target.value)}>
-            </textarea>
-            <button className="btn std-input-btn">
+            <textarea
+              id="code-inp"
+              onChange={(e) => setUserInput(e.target.value)}
+            ></textarea>
+            <button className="btn std-input-btn" onClick={() => compile()}>
               Run
             </button>
           </div>
           <h4>Output:</h4>
-          <div className="output-box">
+          {loading ? (
+            <pre><h4>LOADING...</h4></pre>
+          ) : (
+            <div className="output-box">
               <pre>{userOutput}</pre>
-              <button onClick={() => { clearOutput() }}
-                 className="btn clear-btn">
-                 Clear
+              <button
+                onClick={() => {
+                  clearOutput();
+                }}
+                className="btn clear-btn"
+              >
+                Clear
               </button>
             </div>
-
-
-
-
-            </div>
+          )}
+        </div>
       </div>
     </div>
   );
